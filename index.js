a9daaef56d0f57c836668234a106aa065b8c61b9
AnikaBushra <anikabushra024@gmail.com>
2023-05-22 10:31:35 +0600

    single data get implemented

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
             const result = await newToyCollections.deleteOne(query);
             res.send(result)
         })
+        // single data get 
+        app.get('/addAToy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await newToyCollections.findOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
